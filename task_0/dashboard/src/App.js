a6c55026e1d4c662478a29dcebbc6b521fd1c4b5
Ayooluwa Gideon Oloyede <ayooluwaoloyede@gmail.com>
2024-11-30 11:53:05 +0100

    task-1

--- task_0/dashboard/src/App.js
+++ task_0/dashboard/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import logo from './logo.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} className="App-logo" alt="Logo" />
+        <h1>School Dashboard</h1>
       </header>
+	<hr />
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+      </div>
+	<hr />
+	<div className="App-footer">
+		<p>&copy; 2020 - holberton School</p>
+	</div>
     </div>
   );
 }
 
 export default App;
+
+
+/**in task_0/dashboard/src/App.js, create a function App that returns:
+
+a header div with a class named App-header containing the Holberton logo and a h1 with the text School dashboard
+a body div with a class named App-body containing at least one paragraph with the text Login to access the full dashboard
+a footer div with a class named App-footer containing at least one paragraph with the text Copyright 2020 - holberton School**/
