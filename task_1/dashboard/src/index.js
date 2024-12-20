dfadc1c5da546f6e12ab54849f590a08aae43469
Oloyede Ayooluwa <138716971+Gideon-Esq@users.noreply.github.com>
2024-12-20 14:46:23 +0000

    gg

--- task_1/dashboard/src/index.js
+++ task_1/dashboard/src/index.js
@@ -7,8 +7,8 @@ import Notifications from "./Notifications";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
     <div id="root-notifications">{<Notifications />}</div>
+    <App />
   </React.StrictMode>
 );
 
