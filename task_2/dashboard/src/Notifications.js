f2605ceea28db772d2b1dd45377b7a7c32595489
Oloyede Ayooluwa <138716971+Gideon-Esq@users.noreply.github.com>
2024-12-23 12:32:03 +0000

    Task 2 alx-react

--- task_2/dashboard/src/Notifications.js
+++ task_2/dashboard/src/Notifications.js
@@ -1,10 +1,14 @@
 import React from "react";
 import "./Notifications.css";
+import utils from "./utils.js";
 
 function Notifications() {
   return (
     <div className="Notifications">
       <p>Here is the list of notifications</p>
+      <ul>
+        <li>{utils.getLatestNotification()}</li>
+      </ul>
     </div>
   );
 }
