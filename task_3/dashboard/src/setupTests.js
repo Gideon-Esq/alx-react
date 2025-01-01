f5535a8204c2517a26301caae7b601b10d43e001
Ayooluwa Gideon Oloyede <ayooluwaoloyede71@gmail.com>
2025-01-02 00:18:17 +0100

    delete unnecessary file

--- task_3/dashboard/src/setupTests.js
+++ task_3/dashboard/src/setupTests.js
@@ -1,3 +1,6 @@
+import { TextEncoder, TextDecoder } from "text-encoding";
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
 // jest-dom adds custom jest matchers for asserting on DOM nodes.
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
